750ccdcc9e41bac2b2a443568367146f6d7d5244
Babichev Maxim <maksim.babichev95@gmail.com>
2020-06-07 00:13:55 +0300

    add bavix/laravel-wallet

--- app/User.php
+++ app/User.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use Bavix\Wallet\Traits\HasWallet;
+use Bavix\Wallet\Traits\HasWallets;
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
@@ -11,6 +13,7 @@ use Laravel\Lumen\Auth\Authorizable;
 class User extends Model implements AuthenticatableContract, AuthorizableContract
 {
     use Authenticatable, Authorizable;
+    use HasWallet, HasWallets;
 
     /**
      * The attributes that are mass assignable.
